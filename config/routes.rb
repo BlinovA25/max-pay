10be2da8ca98c9424a17fee17c910a2ac9ff3d1c
anton <BlinovA25>
2023-05-10 18:28:45 +0200

    Added devise for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :lessons
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   # Defines the root path route ("/")
   root "lessons#index"
+
+  resources :lessons
 end
